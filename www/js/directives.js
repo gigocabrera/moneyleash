915b34f345012ef9fbf1dba06ec4c3b431a201cc
Luis Cabrera <gigocabrera@outlook.com>
2015-09-22 21:47:36 -0400

    iOS 9 bug fix - App Transport Security

--- www/js/directives.js
+++ www/js/directives.js
@@ -241,4 +241,51 @@ angular.module('moneyleash.directives', [])
         };
     })
 
+    .directive('ionRadioFix', function () {
+        return {
+            restrict: 'E',
+            replace: true,
+            require: '?ngModel',
+            transclude: true,
+            template:
+              '<label class="item item-radio">' +
+                '<input type="radio" name="radio-group">' +
+                '<div class="radio-content">' +
+                  '<div class="item-content disable-pointer-events" ng-transclude></div>' +
+                  '<i class="radio-icon disable-pointer-events icon ion-checkmark"></i>' +
+                '</div>' +
+              '</label>',
+
+            compile: function (element, attr) {
+                if (attr.icon) {
+                    var iconElm = element.find('i');
+                    iconElm.removeClass('ion-checkmark').addClass(attr.icon);
+                }
+
+                var input = element.find('input');
+                angular.forEach({
+                    'name': attr.name,
+                    'value': attr.value,
+                    'disabled': attr.disabled,
+                    'ng-value': attr.ngValue,
+                    'ng-model': attr.ngModel,
+                    'ng-disabled': attr.ngDisabled,
+                    'ng-change': attr.ngChange,
+                    'ng-required': attr.ngRequired,
+                    'required': attr.required
+                }, function (value, name) {
+                    if (angular.isDefined(value)) {
+                        input.attr(name, value);
+                    }
+                });
+
+                return function (scope, element, attr) {
+                    scope.getValue = function () {
+                        return scope.ngValue || attr.value;
+                    };
+                };
+            }
+        };
+    })
+
 ;
